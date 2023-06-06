3b02cbfdb511e484e5f89ca0067541e9c0fa9bfd
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-06-06 06:53:43 -0700

    print Ascii value in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(25, -1, -1):
-    print("{}{}".format(chr(i + 97), chr(i + 65)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
