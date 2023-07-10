666795e36eb29a1d52ca6e449c11fa3f7d4b795b
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-07-10 02:04:52 -0700

    comment errors fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+0-lookup
+
+Contains method lookup that returns list of object's attribute and methods
+"""
 
 
 def lookup(obj):
+    """returns list of object's attribute and methods"""
     return dir(obj)
