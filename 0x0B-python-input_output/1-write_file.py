be880d80aa78d13a4e3858b97229e2f0e1316262
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-07-11 06:55:02 -0700

    file updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-contain function that writes text and 
-returns number chars written
-"""
+""" define a test file """
+
 
 def write_file(filename="", text=""):
-    """
-    Writes text given into a file `filename`.
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        if text and type(text) is str:
-            return f.write(text)
+    """ write the string to utf8 """
+
+    with open(filename, "w", encoding="utf-8") as fl:
+        return fl.write(text)
