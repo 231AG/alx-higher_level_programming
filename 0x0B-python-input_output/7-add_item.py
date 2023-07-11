dc3eab91d18f7c1b982a5a450afc6cb1fad189c1
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-07-11 06:59:27 -0700

    final commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" script to save and load """
+
+import sys
+
+def main():
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =  __import__('6-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
+
+    l = [sys.argv[i] for i in range(1, len(sys.argv))]
+    try:
+        existing_content = load_from_json_file(filename)
+    except:
+        existing_content = []
+    content = existing_content + l
+    save_to_json_file(content, filename)
+
+main()
