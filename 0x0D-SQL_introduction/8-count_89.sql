55bf3ed74fd77672fcc0822b8ad88442f21a6281
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-08-15 05:14:32 -0700

    count 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,1 +1,4 @@
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+-- displays the number of records with id in the table firsttable in sql server
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
